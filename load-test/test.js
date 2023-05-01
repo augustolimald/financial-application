f6d57fa7f4f6f6ca41da41a89207ce55447b0126
Augusto Lima <augustocesar827@gmail.com>
2023-05-01 11:00:06 -0300

    Add load test

---
+++ load-test/test.js
@@ -0,0 +1,101 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+const url = 'http://localhost:4000';
+
+const version = 'v1';
+
+function generateUser() {
+	const id = Math.random() * 100000000;
+	return {
+		name: `Test-${id}`,
+		email: `test-${id}@mail.com`,
+		password: '1234'
+	};
+}
+
+function generateTransaction(number) {
+	return {
+		description: `${number}`,
+		value: Math.ceil(Math.random() * 2000) - 1000
+	};
+}
+
+export const options = {
+	stages: [
+		{ target: 1 }
+	]
+};
+
+export function setup() {
+	http.post(`${url}/cleanup`);
+}
+
+export default function () {
+	const params = {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		tags: { number: '0'}
+	};
+
+	const userData = generateUser();
+
+	http.post(
+		`${url}/${version}/users`, 
+		JSON.stringify(userData), 
+		params
+	);
+
+	let response = http.post(
+		`${url}/${version}/login`,
+		JSON.stringify({
+			email: userData.email,
+			password: userData.password
+		}),
+		params
+	);
+
+	params.headers.Authorization = `Bearer ${JSON.parse(response.body).token}`;
+
+	let balance = 0;
+
+	const requests = [];
+
+	for(let i = 0; i < 2000; i++) {
+		const transaction = generateTransaction();
+
+		balance += transaction.value;
+
+		const options = {};
+		Object.assign(options, params);
+		options.tags = {number: `${i}`};
+
+		requests.push([
+			'POST',
+			`${url}/${version}/transactions`,
+			JSON.stringify(transaction),
+			options
+		]);
+
+		requests.push([
+			'GET',
+			`${url}/${version}/balance`,
+			null,
+			options
+		]);
+	}
+
+	http.batch(requests);
+
+	sleep(1);
+
+	response = http.get(`${url}/${version}/balance`, params);
+
+	check(response, {
+		'Balance is correct': (r) => {
+			const data = JSON.parse(r.body);
+			return data.balance === balance
+		}
+	});
+}
